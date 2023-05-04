104b6689574906be4e1a9f220878c6c030d8c252
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2023-05-03 23:32:02 -0300

    update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,9 @@
-const btnteste = document.getElementById('btnteste')
 
-btnteste.addEventListener('click', function () {
-    console.log("Clicou no botão");
-})
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((responseBody) => console.log(responseBody))
+    .catch((error) => console.error(error))
