4066af827daa3e0536676bdb51795b80af2d222a
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2023-05-08 11:04:19 -0300

    Desenvolvendo pokedex
    Utilização de map;
    Utilização de requisições url e promisses

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,28 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then((response) => response.json())
-    .then((responseBody) => console.log(responseBody))
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="${pokemon.name}">
+        </div>               
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+    pokeApi.getPokemons().then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+    })
+   
     .catch((error) => console.error(error))
