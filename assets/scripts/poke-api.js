de5d0be50bc80d1dabbc18e92af552ddbfd8af1c
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2023-05-09 10:07:24 -0300

    manipulando lista de pokemons;

--- assets/scripts/poke-api.js
+++ assets/scripts/poke-api.js
@@ -11,7 +11,5 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails)=> {
-        console.log(pokemonsDetails)
-    }) 
+    .then((pokemonsDetails)=> (pokemonsDetails)) 
 }
